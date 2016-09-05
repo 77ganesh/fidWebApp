51fed33e7cf1e78a51f3b823982ccfee66e7084b
கணேஷ் ⚡ ☢ ☺ <   >
2016-09-05 14:34:54 +0530

    hello world json output

--- src/main/java/group/G/gController.java
+++ src/main/java/group/G/gController.java
@@ -2,6 +2,7 @@ package group.G;
 
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,12 @@ public final class gController {
 		//dao.delete(t);
 		model.addAttribute("message", t.toString());
 		return "funny";
+	}
+
+	@RequestMapping("/tj")
+	public @ResponseBody UserModel fun2() {
+		UserModel t = new UserModel("robin","batman","pikachu");
+		return t;
 	}
 
 }
